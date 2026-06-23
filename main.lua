dbc665965a44326bde8d574331c2943d341ce148
Chel1k <pisik3872@gmail.com>
2026-06-23 22:21:13 +0300

    Added cache busting to main.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,11 @@
 -- main.lua
 -- Основной файл загрузки (точка входа)
 
--- Загрузка скриптов напрямую с GitHub
-local ui = loadstring(game:HttpGet("https://raw.githubusercontent.com/Chel1k-1337/grow-a-garden-18238123/main/ui.lua"))()
-local autobuy = loadstring(game:HttpGet("https://raw.githubusercontent.com/Chel1k-1337/grow-a-garden-18238123/main/autobuy.lua"))()
+-- Загрузка скриптов напрямую с GitHub (добавлен ?v=tick() для обхода кэша Гитхаба)
+local url_ui = "https://raw.githubusercontent.com/Chel1k-1337/grow-a-garden-18238123/main/ui.lua?v=" .. tostring(tick())
+local url_autobuy = "https://raw.githubusercontent.com/Chel1k-1337/grow-a-garden-18238123/main/autobuy.lua?v=" .. tostring(tick())
+
+local ui = loadstring(game:HttpGet(url_ui))()
+local autobuy = loadstring(game:HttpGet(url_autobuy))()
 
 ui:Init(autobuy)
